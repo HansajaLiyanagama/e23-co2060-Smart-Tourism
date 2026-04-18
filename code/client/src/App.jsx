796c362d15ae6dae2480e3cf227dab55b8401363
Hansaja Liyanagama <dihanhansajaliyanagama@gmail.com>
2026-04-18 12:40:40 +0530

    feat: restructure App component to implement routing with Login and Home pages

--- code/client/src/App.jsx
+++ code/client/src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div>
-      <h1>Smart Tourism System</h1>
-      <p>Frontend successfully connected!</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* The default route (http://localhost:5173/) will show the Login page */}
+        <Route path="/" element={<Login />} />
+        
+        {/* The /home route (http://localhost:5173/home) will show the Map page */}
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
