cb3a68fdcdb776b50e61ed3c66202e52c725fbc6
Hansaja Liyanagama <dihanhansajaliyanagama@gmail.com>
2026-04-18 12:36:45 +0530

    feat: add Home and Login components for user navigation

---
+++ code/client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Tourist Map</h2>
+      <p>Welcome to the Smart Tourism System!</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ code/client/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <p>Please enter your credentials.</p>
+    </div>
+  );
+}
+
+export default Login;
